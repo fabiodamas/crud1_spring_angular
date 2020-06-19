17667a4dbb2622489217c6095a2c8089985864ee
fabio damas <fabio.damas@gmail.com>
2020-06-19 16:33:03 -0300

    servicos, rotas, classes

--- Employee-Crud/src/app/app.module.ts
+++ Employee-Crud/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { UpdateEmployeeComponent } from './components/update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeeListComponent,
+    EmployeeDetailsComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     BrowserModule,
---
+++ Employee-Crud/src/app/services/employee.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private url= 'http://localhost:8080/springboot-crud-rest/api/path1/employees';
+
+  constructor(private http: HttpClient) { }
+
+  getEmployee(id: number): Observable<any> {
+    return this.http.get(`${this.url}/${id}`);
+  }
+
+  createEmployee(employee: Object): Observable<Object> {
+    return this.http.post(`${this.url}`, employee);
+  }
+
+  updateEmployee(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.url}/${id}`, value);
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    return this.http.delete(`${this.url}/${id}`, { responseType: 'text' });
+  }
+
+  getEmployeesList(): Observable<any> {
+    return this.http.get(`${this.url}`);
+  }
+}
