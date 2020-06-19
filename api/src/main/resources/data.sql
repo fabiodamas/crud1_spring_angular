6fa30e88a8720fe6b1997e2ccdff2c199c020e71
fabio damas <fabio.damas@gmail.com>
2020-06-19 11:45:37 -0300

    atualiando o data.sql

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,13 +1,16 @@
-DROP TABLE IF EXISTS billionaires;
- 
-CREATE TABLE billionaires (
+CREATE TABLE IF NOT EXISTS billionaires (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   first_name VARCHAR(250) NOT NULL,
   last_name VARCHAR(250) NOT NULL,
   career VARCHAR(250) DEFAULT NULL
 );
  
-INSERT INTO billionaires (first_name, last_name, career) VALUES
-  ('Aliko', 'Dangote', 'Billionaire Industrialist'),
-  ('Bill', 'Gates', 'Billionaire Tech Entrepreneur'),
-  ('Folrunsho', 'Alakija', 'Billionaire Oil Magnate');
+INSERT INTO billionaires (first_name, last_name, career)
+SELECT 'fabio','damas','analista de sistemas'
+WHERE NOT EXISTS (SELECT * FROM billionaires where first_name='fabio') ;
+
+INSERT INTO billionaires (first_name, last_name, career)
+SELECT 'ludmila','garcia', 'biologa'
+WHERE NOT EXISTS (SELECT * FROM billionaires where first_name='ludmila') ;
+
+ 
